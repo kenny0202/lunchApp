972576ccf6ca2d1e9b85544e20c32b1765a27e56
Kenny <kenny020259@hotmail.com>
2016-01-19 22:40:33 -0800

    Fixed random shuffle & added hardcoded more restaurants

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -4,47 +4,119 @@ app.controller('RestaurantCtrl', function($scope, $timeout, $http) {
       {
         "name": "Delicious Pho",
         "budget": "$10",
-        "phone": "604-512-0433",
-        "address": "4049 Cherry Lane Charleston, SC 29406",
+        "phone": "(604) 559-4088",
+        "address": "255 Robson St, Vancouver, BC V6B 0E8",
         "img": "img/delicious.jpg",
         "cuisine": "Vietnamese",
-        "rating": "5" 
+        "rating": "7" 
       },
       {
         "name": "Jinya Ramen",
         "budget": "$12",
-        "phone": "604-346-1235",
-        "address": "265 School Street Sebastian, FL 32958",
+        "phone": "(604) 568-9711",
+        "address": "270 Robson St, Vancouver, BC V6B 0E7",
         "img": "img/jinya.jpg",
         "cuisine": "Japanese",
-        "rating": "10" 
+        "rating": "8" 
       },
       {
-        "name": "Italian Kitchen",
-        "budget": "$15",
-        "phone": "604-324-8709",
-        "address": "6462 Marshall Street Youngstown, OH 44512",
-        "img": "img/italian.jpg",
-        "cuisine": "Italian",
-        "rating": "9" 
+        "name": "Super Chef Grill",
+        "budget": "$7",
+        "phone": "(604) 681-7189",
+        "address": "280 Robson St, Vancouver, BC V6B 6A1",
+        "img": "img/super.jpg",
+        "cuisine": "Chinese",
+        "rating": "7" 
       },
       {
         "name": "Chipotle",
         "budget": "$9",
-        "phone": "604-902-4182",
-        "address": "5464 Lincoln Avenue Fairfax, VA 22030",
+        "phone": "(604) 683-6394",
+        "address": "818 Howe St, Vancouver, BC V6Z 1N4",
         "img": "img/chipotle.png",
         "cuisine": "Mexican",
-        "rating": "7" 
+        "rating": "8" 
       },
       {
-        "name": "Blue Water Cafe",
-        "budget": "$30",
-        "phone": "604-321-5234",
-        "address": "3700 Depot Street Oxnard, CA 93035",
-        "img": "img/blue.jpg",
+        "name": "Grounds For Appeal",
+        "budget": "$9",
+        "phone": "(604) 681-8558",
+        "address": "845 Cambie St, Vancouver, BC V6B",
+        "img": "img/grounds.jpg",
+        "cuisine": "Indian",
+        "rating": "8" 
+      },
+      {
+        "name": "Patron Tacos & Cantina",
+        "budget": "$12",
+        "phone": "(604) 558-3368",
+        "address": "265 Robson St, Vancouver, BC V6B 6A1",
+        "img": "img/patron.jpg",
+        "cuisine": "Mexican",
+        "rating": "8" 
+      },
+      {
+        "name": "Hamilton Street Grill",
+        "budget": "$11",
+        "phone": "(604) 331-1511",
+        "address": "1009 Hamilton St, Vancouver, BC V6B 6K6",
+        "img": "img/hamilton.png",
         "cuisine": "Western",
-        "rating": "10" 
+        "rating": "8" 
+      },
+      {
+        "name": "Rice N' Spice",
+        "budget": "$9",
+        "phone": "(604) 568-4446",
+        "address": "883 Hamilton St, Vancouver, BC V6R 2R7",
+        "img": "img/rice.jpg",
+        "cuisine": "Indian",
+        "rating": "8" 
+      },
+      {
+        "name": "Fresh Bowl",
+        "budget": "$10",
+        "phone": "(604) 688-8565",
+        "address": "1128 Pacific Blvd, Vancouver, BC V6Z 2X7",
+        "img": "img/fresh.jpg",
+        "cuisine": "South East Asian",
+        "rating": "8" 
+      },
+      {
+        "name": "DD Mau",
+        "budget": "$9",
+        "phone": "(604) 684-4446",
+        "address": "1239 Pacific Blvd, Vancouver, BC V6Z 2V2",
+        "img": "img/dd.jpg",
+        "cuisine": "Vietnamese",
+        "rating": "8" 
+      },
+      {
+        "name": "Yopo Cafe",
+        "budget": "$11",
+        "phone": "(604) 609-9676",
+        "address": "1122 Homer St, Vancouver, BC V6B 2X6",
+        "img": "img/yopo.gif",
+        "cuisine": "Chinese",
+        "rating": "8" 
+      },
+      {
+        "name": "Sushi Maro",
+        "budget": "$11",
+        "phone": "(778) 371-8332",
+        "address": "1168 Hamilton St #101, Vancouver, BC V6B 2S2",
+        "img": "img/maro.jpg",
+        "cuisine": "Japanese",
+        "rating": "8" 
+      },
+      {
+        "name": "Urban Thai Bistro",
+        "budget": "$11",
+        "phone": "(604) 408-7788",
+        "address": "1119 Hamilton St, Vancouver, BC V6B 5P6",
+        "img": "img/urban.jpg",
+        "cuisine": "Thai",
+        "rating": "8" 
       }
     ];
 
@@ -61,7 +133,7 @@ app.controller('RestaurantCtrl', function($scope, $timeout, $http) {
       //$timeout to allow animation to complete before changing to the next product
       $timeout(function() {
         //get index of random restaurant 
-        var randomRestaurantIndex = Math.round(Math.random() * ($scope.restaurants.length - 1));
+        var randomRestaurantIndex = Math.floor(Math.random() * ($scope.restaurants.length));
 
         //update current restaurant in scope
         $scope.currentRestaurant = angular.copy($scope.restaurants[randomRestaurantIndex]);
